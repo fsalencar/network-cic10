55a6f651f695b6dfc15c7b831e194a01dc2652eb
EC2 Default User <ec2-user@ip-10-0-2-228.ec2.internal>
2023-09-15 01:16:15 +0000

    Changing tag name

--- prod/main.tf
+++ prod/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
     default_tags {
         tags = {
             Owner   = "Felipe Alencar"
-            Env     = "Producao"
+            Env     = "Producao-ambiente"
             Class   = "CLC10"
         }
     }
